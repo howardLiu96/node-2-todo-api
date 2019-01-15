1c14d42ba2edc64fa9c78ebd64aa4fa35bda0472
Howard <hliu1461603525@gmail.com>
2019-01-15 18:54:28 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,13 +8,22 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
 	});
-	
+
 	todo.save().then((doc) => {
 		res.send(doc);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos: todos});
 	}, (err) => {
 		res.status(400).send(err);
 	});
