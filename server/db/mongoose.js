acbddd564fe966937ce6e67698c21ea73ac1d182
Howard <hliu1461603525@gmail.com>
2019-01-16 01:39:56 -0500

    Fix the wrong URL of mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 let db = {
 	localhost: 'mongodb://localhost:27017/TodoApp',
-	mlab: 'mongodb://rs146160:NoIdea149@ds159164.com:59164/howard_todo'
+	mlab: 'mongodb://rs146160:NoIdea149@ds159164.mlab.com:59164/howard_todo'
 };
 mongoose.Promise = global.Promise;
 mongoose.connect(db.localhost || db.mlab);
