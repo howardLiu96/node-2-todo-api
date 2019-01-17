a2e34b7a38689bdb5da927b1929e9f9579f4cbfc
Howard <hliu1461603525@gmail.com>
2019-01-17 00:59:16 -0500

    Fix test1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,9 @@ let db = {
 	mlab: 'mongodb://howard:asdf1234@ds159164.mlab.com:59164/howard_todo'
 };
 mongoose.Promise = global.Promise;
-mongoose.connect(db.mlab || db.localhost);
+mongoose.connect(db.localhost).then((db) => {}, (err) => {
+	mongoose.connect(db.mlab);
+});
 
 module.exports = {
 	mongoose: mongoose
