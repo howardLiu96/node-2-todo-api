ae895c385f00c50bcb69421f0311dde56e5956b4
Howard <hliu1461603525@gmail.com>
2019-01-17 03:51:46 -0500

    Fix db 1st

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if (env == 'development') {
 } else if (env === 'test') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+	process.env.MONGODB_URI = 'mongodb://howard:asdf1234@ds159164.mlab.com:59164/howard_todo';
 }
