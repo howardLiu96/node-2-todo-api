e7721ce349e62631727863c537ac705885b991c1
Howard <hliu1461603525@gmail.com>
2019-01-17 03:48:30 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,11 @@ let db = {
 	mlab: 'mongodb://howard:asdf1234@ds159164.mlab.com:59164/howard_todo'
 };
 mongoose.Promise = global.Promise;
-mongoose.connect(db.localhost).then((db) => {}, (err) => {
-	mongoose.connect(db.mlab);
-});
+// mongoose.connect(db.localhost).then((db) => {}, (err) => {
+// 	mongoose.connect(db.mlab);
+// });
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
 	mongoose: mongoose
